a05588278bda679261b01073545b3abc72b50291
Yuridia Larios Aispuro <yuriydani@gmail.com>
2017-07-20 17:50:17 -0700

    Update basicAlgorithmScripting.js

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -234,3 +234,25 @@ function slasher(arr, howMany) {
 }
 */
 
+
+
+
+/*Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.*/
+
+function mutation(arr) {
+ var allLetters = true;
+ var word1 = arr[0].toLowerCase();
+ var word2 = arr[1].toLowerCase();
+  
+ for(var i=0; i< word2.length; i++){
+  if(word1.indexOf(word2.charAt(i)) == -1){
+    allLetters = false;
+  }
+ } 
+  
+  return allLetters;
+}
+
+mutation(["floor", "for"]);
+
