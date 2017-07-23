0bdb0e2a8fefd0d1853417e7a37901e2768ebd65
Yuridia Larios Aispuro <yuriydani@gmail.com>
2017-07-22 19:02:21 -0700

    Update basicAlgorithmScripting.js

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -256,3 +256,33 @@ function mutation(arr) {
 
 mutation(["floor", "for"]);
 
+
+
+
+/*
+Remove all falsy values from an array.
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+*/
+
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  
+  return arr.filter(function(value){
+      return Boolean(value);     
+});
+  
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+
+
+/*
+advance coding solution:
+
+function bouncer(arr) {
+  return arr.filter(Boolean);
+}
+
+*/
+
